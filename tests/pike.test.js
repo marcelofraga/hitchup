1eab1a70478d2afabcaa95321fba7f23de63d9bf
Marcelo Fraga <marcelo.fraga@me.com>
2017-08-06 21:26:41 -0300

    Add tests to Pike instance

--- tests/pike.test.js
+++ tests/pike.test.js
@@ -1,7 +1,56 @@
 import Pike from '../src/pike';
 
+const data = {
+  user: {
+    isHidden: true
+  }
+};
+
+let instance;
+let dummy;
+let element;
+
+class Dummy {
+  constructor(element, data) {
+    this.user = data.user;
+  }
+}
+
+beforeEach(() => {
+  document.body.innerHTML = `
+    <div class="user">
+      <span class="user-name" data-hide="user.isHidden"></span>
+    </div>
+  `;
+
+  element = document.querySelector('.user');
+  instance = new Pike(element, data);
+  dummy = new Dummy(element, data);
+});
+
 describe('Pike', () => {
-  it('should be defined', () => {
-    expect(Pike).toBeDefined();
+  it('throws type error when does not pass an element', () => {
+    expect(() => new Pike('.user')).toThrowError('must be an element or text node');
+  });
+
+  it('sets element and model', () => {
+    expect(instance.element).toBe(element);
+    expect(instance.model).toBe(data);
+  });
+
+  it('run binders by the declarative data binding of the element', () => {
+    const child = element.children[0];
+    expect(child.style.display).toEqual('none');
+  });
+
+  describe('when model is a class instance', () => {
+    beforeEach(() => {
+      instance = new Pike(element, dummy);
+    });
+
+    it('run binder with instance as data model', () => {
+      const child = element.children[0];
+      expect(child.style.display).toEqual('none');
+    });
   });
 });
