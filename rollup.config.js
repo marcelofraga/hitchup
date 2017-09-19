e10c9c071fb2b675b7741d606ea2669a399f6755
Marcelo Fraga <marcelo.fraga@me.com>
2017-09-18 23:28:48 -0300

    Change rollup module name

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,7 @@ import babel from 'rollup-plugin-babel';
 export default {
   entry: 'src/index.js',
   format: 'umd',
-  moduleName: 'Hitchup',
+  moduleName: 'hitchup',
   plugins: [
     resolve(),
     commonjs(),
